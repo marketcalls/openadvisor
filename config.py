9acd5898495c58bec568c46ccc54c9689f5499eb
marketcalls <rajandran@marketcalls.in>
2024-07-27 21:55:31 +0530

    preparing to merge with deepanshus user management

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance', 'finance_symbols.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'your_secret_key_here'
-    SCHEDULER_API_ENABLED = True
